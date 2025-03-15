4c9f550594c57a7d7d41757aa303ff4c7bb5d654
Sangeeth Sulochana <ssulochanaherath@gmail.com>
2025-03-15 10:48:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import Login from "./pages/Login.tsx";
 import {RootLayout} from "./components/RootLayout.tsx";
+import Signup from "./pages/Signup.tsx";
 
 function App() {
     const router = createBrowserRouter([
@@ -9,8 +10,8 @@ function App() {
             path: "/",
             element: <RootLayout />,
             children: [
-                {
-                    path: "/", element: <Login /> },
+                { path: "/", element: <Login /> },
+                { path: "/signup", element: <Signup /> },
             ],
         },
     ]);
